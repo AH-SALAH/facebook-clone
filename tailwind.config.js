2684b0ff149bf4926edcf4aea16e68ff53fccda7
AH-SALAH <ah.9alah@gmail.com>
2022-02-15 22:15:24 +0400

    ✨ feat: add slidein anime

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            animation: {
+                slidein: 'slidein 0.2s ease forwards',
+            },
+            keyframes: {
+                slidein: {
+                    '0%': { transform: 'translate(0,20%)', opacity: 0 },
+                    '100%': { transform: 'translate(0,0)', opacity: 1 },
+                }
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/line-clamp'),
+    ],
 }
